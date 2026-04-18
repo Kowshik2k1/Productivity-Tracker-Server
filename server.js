354e28235dc229f5922ceaab862629390099a0db
Kowshik <koushikvarmabalaraju369@gmail.com>
2026-04-18 18:05:42 +0530

    Inital commit with test route

---
+++ server.js
@@ -0,0 +1,18 @@
+import cors from "cors";
+import mongoose from "mongoose";
+import { PORT, MONGO_URI } from "./src/config/db.js";
+import { app } from "./app.js";
+import testRoutes from "./src/routes/testRoutes.js";
+
+app.use(cors());
+
+mongoose.connect(MONGO_URI)
+.then(() => {
+    console.log('MongoDB connected');
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+})
+.catch((err) => console.error('MongoDB connection error:', err));
+
+app.use('/api/test', testRoutes);
